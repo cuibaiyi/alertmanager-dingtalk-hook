c666cddf595505339ef60fe72da31a03848c8ec7
carlos <619876714@qq.com>
2019-04-12 11:54:03 +0800

    cby update

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 import os
 import json
 import requests
-
-from flask import Flask
-from flask import request
+from flask import Flask, request
 
 app = Flask(__name__)
-
 
 @app.route('/', methods=['POST', 'GET'])
 def send():
@@ -17,17 +14,16 @@ def send():
     else:
         return 'weclome to use prometheus alertmanager dingtalk webhook server!'
 
-
 def bytes2json(data_bytes):
     data = data_bytes.decode('utf8').replace("'", '"')
     return json.loads(data)
-
 
 def send_alert(data):
     token = os.getenv('ROBOT_TOKEN')
     if not token:
         print('you must set ROBOT_TOKEN env')
-        return
+        return None
+    
     url = 'https://oapi.dingtalk.com/robot/send?access_token=%s' % token
     send_data = {
         "msgtype": "text",
@@ -39,7 +35,6 @@ def send_alert(data):
     result = req.json()
     if result['errcode'] != 0:
         print('notify dingtalk error: %s' % result['errcode'])
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
